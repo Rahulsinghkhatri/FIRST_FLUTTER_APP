61d2070460b194075cf928dbc2de91e7ec3ab81e
RAHUL SINGH KHATRI <rahulsinghkhatri2002@gmail.com>
2022-03-26 13:53:55 +0530

    day4 completed

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      //home: HomePage(),      ////Either we will use this or use route to define homepage
+      themeMode: ThemeMode.light, //if here thememode.dark the it will be dark
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+          brightness: Brightness
+              .dark //it will overide the deepPurple color so the theme will be dark
+          ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => HomePage(),
+        "/home": (context) => HomePage(),
+        "/Login": (context) => LoginPage()
+      },
     );
   }
 }
